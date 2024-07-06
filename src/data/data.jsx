aa4f2abe389ed7cf887a02d4a3bb45d6c83ab4bb
Mahmoud Elsherbiny <mami55209@gmail.com>
2024-07-06 19:35:15 +0300

    project done

---
+++ src/data/data.jsx
@@ -0,0 +1,655 @@
+import p1 from "../assets/1.svg";
+import p2 from "../assets/2.svg";
+import p3 from "../assets/3.svg";
+import p4 from "../assets/4.svg";
+import p5 from "../assets/5.svg";
+import p6 from "../assets/6.svg";
+import p7 from "../assets/7.svg";
+import p8 from "../assets/8.svg";
+import p9 from "../assets/9.svg";
+import p10 from "../assets/10.svg";
+import p11 from "../assets/11.svg";
+import p12 from "../assets/12.svg";
+import p13 from "../assets/13.svg";
+import p14 from "../assets/14.svg";
+
+
+
+import p15 from "../assets/15.png";
+import p19 from "../assets/19.png";
+import p20 from "../assets/20.png";
+import p21 from "../assets/21.png";
+import p22 from "../assets/22.png";
+import p23 from "../assets/23.png";
+import p30 from "../assets/30.png";
+import p31 from "../assets/31.png";
+import p32 from "../assets/32.png";
+import p33 from "../assets/33.png";
+import p34 from "../assets/34.png";
+
+
+
+
+/* cards */
+import a1 from "../assets/card/1-a.jpg";
+import b1 from "../assets/card/1-b.jpg";
+import c1 from "../assets/card/1-c.jpg";
+import d1 from "../assets/card/1-d.jpg";
+
+import a2 from "../assets/card/2-a.jpg";
+import b2 from "../assets/card/2-b.jpg";
+import c2 from "../assets/card/2-c.jpg";
+import d2 from "../assets/card/2-d.jpg";
+import e2 from "../assets/card/2-e.jpg";
+import f2 from "../assets/card/2-f.jpg";
+
+
+import a3 from "../assets/card/3-a.jpg";
+
+import a4 from "../assets/card/4-a.jpg";
+import b4 from "../assets/card/4-b.jpg";
+import c4 from "../assets/card/4-c.jpg";
+
+import a5 from "../assets/card/5-a.jpg";
+import b5 from "../assets/card/5-b.jpg";
+import a6 from "../assets/card/6-a.jpg";
+import b6 from "../assets/card/6-b.jpg";
+import a7 from "../assets/card/7-a.jpg";
+import b7 from "../assets/card/7-b.jpg";
+import a8 from "../assets/card/8-a.jpg";
+import b8 from "../assets/card/8-b.jpg";
+import a9 from "../assets/card/9-a.jpg";
+import b9 from "../assets/card/9-b.jpg";
+import a10 from "../assets/card/10-a.jpg";
+
+import a11 from "../assets/card/11-a.jpg";
+
+import a12 from "../assets/card/12-a.jpg";
+
+import a13 from "../assets/card/13-a.jpg";
+import b13 from "../assets/card/13-b.jpg";
+import a14 from "../assets/card/14-a.jpg";
+import b14 from "../assets/card/14-b.jpg";
+import c14 from "../assets/card/14-c.jpg";
+import d14 from "../assets/card/14-d.jpg";
+
+import a15 from "../assets/card/15-a.jpg";
+
+
+
+import a19 from "../assets/card/19-a.jpg";
+import b19 from "../assets/card/19-b.jpg";
+import a20 from "../assets/card/20-a.jpg";
+
+import a21 from "../assets/card/21-a.jpg";
+import b21 from "../assets/card/21-b.jpg";
+import a22 from "../assets/card/22-a.jpg";
+import b22 from "../assets/card/22-b.jpg";
+import a23 from "../assets/card/23-a.jpg";
+import b23 from "../assets/card/23-b.jpg";
+
+
+import a30 from "../assets/card/30-a.jpg";
+
+import a31 from "../assets/card/31-a.jpg";
+
+import a32 from "../assets/card/32-a.jpg";
+
+import a33 from "../assets/card/33-a.jpg";
+
+import a34 from "../assets/card/34-a.jpg";
+
+
+
+
+
+
+const pics = [
+    {   
+        no: 1,
+        pic: p1,
+        a: a1,
+        b: b1,
+        c: c1,
+        d: d1,
+        double: true
+    },
+    {   
+        no: 2,
+        pic: p2,
+        a: a2,
+        b: b2,
+        c: c2,
+        d: d2,
+        e: e2,
+        f: f2,
+        double: true
+    },
+    {   
+        no: 3,
+        pic: p3,
+        a: a3,
+        double: true
+    },
+    {   
+        no: 4,
+        pic: p4,
+        a: a4,
+        b: b4,
+        c: c4,
+        double: true
+    },
+    {   
+        no: 5,
+        pic: p5,
+        a: a5,
+        b: b5,
+        double: true
+    },
+    {   
+        no: 6,
+        pic: p6,
+        a: a6,
+        b: b6,
+        double: true
+    },
+    {   
+        no: 7,
+        pic: p7,
+        a: a7,
+        b: b7,
+        double: false
+    },
+    {   
+        no: 8,
+        pic: p8,
+        a: a8,
+        b: b8,
+        double: true
+    },
+    {   
+        no: 9,
+        pic: p9,
+        a: a9,
+        b: b9,
+        double: true
+    },
+    {   
+        no: 10,
+        pic: p10,
+        a: a10,
+
+        double: true
+    },
+    {   
+        no: 11,
+        pic: p11,
+        a: a11,
+
+        double: true
+    },
+    {   
+        no: 12,
+        pic: p12,
+        a: a12,
+        double: true
+    },
+    {   
+        no: 13,
+        pic: p13,
+        a: a13,
+        b: b13,
+        double: true
+    },
+    {   
+        no: 14,
+        pic: p14,
+        a: a14,
+        b: b14,
+        c: c14,
+        d: d14,
+ 
+        double: true
+    },
+    {   
+        no: 15,
+        pic: p15,
+        a: a15,
+        double: false
+    },
+    {   
+        no: 19,
+        pic: p19,
+        a: a19,
+        b: b19,
+        double: false
+    },
+    {   
+        no: 20,
+        pic: p20,
+        a: a20,
+        double: true
+    },
+    {   
+        no: 21,
+        pic: p21,
+        a: a21,
+        b: b21,
+        double: true
+    },
+    {   
+        no: 22,
+        pic: p22,
+        a: a22,
+        b: b22,
+        double: false
+    },
+    {   
+        no: 23,
+        pic: p23,
+        a: a23,
+        b: b23,
+        double: true
+    },
+    {   
+        no: 30,
+        pic: p23,
+        a: a23,
+        double: true
+    },
+    {   
+        no: 31,
+        pic: p31,
+        a: a31,
+        double: true
+    },
+    {   
+        no: 32,
+        pic: p32,
+        a: a32,
+        double: true
+    },
+    {   
+        no: 33,
+        pic: p33,
+        a: a33,
+        double: true
+    },
+    {   
+        no: 34,
+        pic: p34,
+        a: a34,
+
+        double: true
+    }
+];
+const canvasSizes = [
+    {   
+        no: 1,
+        a: {
+            x1: 850,
+            y1: 830,
+            x2: 850,
+            y2: 345
+        },
+        b: {
+            x1: 850,
+            y1: 840,
+            x2: 850,
+            y2: 345
+        },
+        c: {
+            x1: 820,
+            y1: 820,
+            x2: 820,
+            y2: 325
+        },
+        d: {
+            x1: 820,
+            y1: 825,
+            x2: 820,
+            y2: 325
+        },
+    },
+    {   
+        no: 2,
+        a: {
+            x1: 860,
+            y1: 735,
+            x2: 860,
+            y2: 250
+        },
+        b: {
+            x1: 860,
+            y1: 735,
+            x2: 860,
+            y2: 250
+        },
+        c: {
+            x1: 860,
+            y1: 750,
+            x2: 860,
+            y2: 335
+        },
+        d: {
+            x1: 860,
+            y1: 720,
+            x2: 860,
+            y2: 355
+        },
+        e: {
+            x1: 860,
+            y1: 720,
+            x2: 860,
+            y2: 355
+        },
+        f: {
+            x1: 860,
+            y1: 720,
+            x2: 860,
+            y2: 355
+        },
+    },
+    {   
+        no: 3,
+        a: {
+            x1: 850,
+            y1: 815,
+            x2: 850,
+            y2: 305
+        },
+    },
+    {   
+        no: 4,
+        a: {
+            x1: 510,
+            y1: 815,
+            x2: 740,
+            y2: 355
+        },
+        b: {
+            x1: 510,
+            y1: 845,
+            x2: 780,
+            y2: 325
+        },
+        c: {
+            x1: 810,
+            y1: 840,
+            x2: 830,
+            y2: 345
+        },
+    },
+    {   
+        no: 5,
+        a: {
+            x1: 510,
+            y1: 800,
+            x2: 510,
+            y2: 285
+        },
+        b: {
+            x1: 510,
+            y1: 800,
+            x2: 510,
+            y2: 285
+        },
+    },
+    {   
+        no: 6,
+        a: {
+            x1: 500,
+            y1: 810,
+            x2: 500,
+            y2: 265
+        },
+        b: {
+            x1: 540,
+            y1: 800,
+            x2: 540,
+            y2: 320
+        }
+    },
+    {   
+        no: 7,
+        a: {
+            x1: 540,
+            y1: 800,
+            x2: 900,
+            y2: 840
+        },
+        b: {
+            x1: 540,
+            y1: 800,
+            x2: 520,
+            y2: 830
+        },
+    },
+    {   
+        no: 8,
+        a: {
+            x1: 830,
+            y1: 810,
+            x2: 830,
+            y2: 260
+        },
+        b: {
+            x1: 810,
+            y1: 805,
+            x2: 810,
+            y2: 340
+        },
+    },
+    {   
+        no: 9,
+        a: {
+            x1: 680,
+            y1: 825,
+            x2: 680,
+            y2: 335
+        },
+        b: {
+            x1: 680,
+            y1: 825,
+            x2: 680,
+            y2: 335
+        }
+    },
+    {   
+        no: 10,
+        a: {
+            x1: 680,
+            y1: 835,
+            x2: 680,
+            y2: 325
+        },
+    },
+    {   
+        no: 11,
+        a: {
+            x1: 600,
+            y1: 825,
+            x2: 745,
+            y2: 260
+        }
+    },
+    {   
+        no: 12,
+        a: {
+            x1: 800,
+            y1: 825,
+            x2: 815,
+            y2: 260
+        }
+    },
+    {   
+        no: 13,
+        a: {
+            x1: 755,
+            y1: 850,
+            x2: 755,
+            y2: 270
+        },
+        b: {
+            x1: 825,
+            y1: 820,
+            x2: 835,
+            y2: 300
+        }
+    },
+    {   
+        no: 14,
+        a: {
+            x1: 780,
+            y1: 805,
+            x2: 780,
+            y2: 340
+        },
+        b: {
+            x1: 760,
+            y1: 805,
+            x2: 760,
+            y2: 340
+        },
+        c: {
+            x1: 790,
+            y1: 805,
+            x2: 790,
+            y2: 340
+        },
+        d: {
+            x1: 845,
+            y1: 800,
+            x2: 845,
+            y2: 400
+        },
+    },
+    {   
+        no: 15,
+        a: {
+            x1: 855,
+            y1: 860,
+            x2: 845,
+            y2: 400
+        },
+    },
+    {   
+        no: 19,
+        a: {
+            x1: 760,
+            y1: 815,
+            x2: 805,
+            y2: 250
+        },
+        b: {
+            x1: 760,
+            y1: 815,
+            x2: 805,
+            y2: 250
+        }
+    },
+    {   
+        no: 20,
+        a: {
+            x1: 840,
+            y1: 815,
+            x2: 840,
+            y2: 375
+        }
+    },
+    {   
+        no: 21,
+        a: {
+            x1: 840,
+            y1: 815,
+            x2: 750,
+            y2: 375
+        },
+        b: {
+            x1: 840,
+            y1: 815,
+            x2: 750,
+            y2: 375
+        }
+    },
+    {   
+        no: 22,
+        a: {
+            x1: 840,
+            y1: 815,
+            x2: 750,
+            y2: 375
+        },
+        b: {
+            x1: 840,
+            y1: 815,
+            x2: 750,
+            y2: 375
+        }
+    },
+    {   
+        no: 23,
+        a: {
+            x1: 840,
+            y1: 830,
+            x2: 750,
+            y2: 375
+        },
+        b: {
+            x1: 840,
+            y1: 830,
+            x2: 750,
+            y2: 375
+        }
+    },
+    {   
+        no: 30,
+        a: {
+            x1: 840,
+            y1: 830,
+            x2: 750,
+            y2: 375
+        },
+    },
+    {   
+        no: 31,
+        a: {
+            x1: 840,
+            y1: 835,
+            x2: 800,
+            y2: 320
+        },
+    },
+    {   
+        no: 32,
+        a: {
+            x1: 840,
+            y1: 825,
+            x2: 800,
+            y2: 355
+        },
+    },
+    {   
+        no: 33,
+        a: {
+            x1: 840,
+            y1: 825,
+            x2: 850,
+            y2: 355
+        },
+
+    },
+    {   
+        no: 34,
+        a: {
+            x1: 875,
+            y1: 825,
+            x2: 875,
+            y2: 435
+        },
+
+    }
+];
+
+export {canvasSizes}
+export default  pics;
